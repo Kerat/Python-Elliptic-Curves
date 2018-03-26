b1b74975509e8bde47c22f8f38b07858b1d399ba
Swiraft <swiraft@gmail.com>
2018-03-26 11:44:24 +0200

    Doc

--- tests/test_diffieHellman.py
+++ tests/test_diffieHellman.py
@@ -1,15 +1,20 @@
 #coding: utf8
+"""
+Authors : MARCE Tarek - GHELIS Amin
+Version : 0.1
+"""
 from unittest import TestCase
-from src.DiffieHellman import *
+from src.DiffieHellman import DiffieHellman
+
 
 class TestDiffieHellman(TestCase):
     def test_get_shared_key(self):
-        alice = DiffieHelman()
+        alice = DiffieHellman.DiffieHelman()
         self.assertEqual(alice.get_shared_key(),alice.shared_key)
 
     def test_echange_secret(self):
-        alice = DiffieHelman()
-        bob = DiffieHelman()
+        alice = DiffieHellman.DiffieHelman()
+        bob = DiffieHellman.DiffieHelman()
         alice.compute_secret(bob.get_shared_key())
         alice_secret = alice.shared_secret
         bob.compute_secret(alice.get_shared_key())
